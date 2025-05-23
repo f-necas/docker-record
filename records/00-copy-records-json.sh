d109e56ff4093df33c50e85ebfae588b916f392d
Florian Necas <florian.necas@camptocamp.com>
2025-05-23 07:45:08 +0200

    bump to ES 8

--- records/00-copy-records-json.sh
+++ records/00-copy-records-json.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cp /docker-entrypoint.d/records.json /mnt/geonetwork_data/config/index/records.json
+#cp /docker-entrypoint.d/features.json /mnt/geonetwork_data/config/index/features.json
 echo ""
 echo "Update records.json."
